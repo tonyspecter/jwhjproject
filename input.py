1cdc70e39fa44dc27e09883d96add7679202d2e8
tonyspecter <tony.specter.choi@gmail.com>
2021-03-31 00:03:05 +0900

    공금입력후 분담금계산 완료

--- input.py
+++ input.py
@@ -8,12 +8,20 @@ for i in range(tot_head):
     name_list.append(name)
 public_money_dict=dict.fromkeys(name_list)
 
-
+#공금지불액 조사
 print('공금지불액을 조사하겠습니다')
+sum=0
 for i in public_money_dict:
     paid_money=int(input('%s :' % i))
+    sum+=paid_money
     public_money_dict[i]=paid_money
 
-from collections import Counterunter
-contribution=Counter(public_money_dict)
-print(contribution)
+#공급지불액 편차 계산
+contribution=int(sum/tot_head)
+public_money_deviation=public_money_dict.copy()
+for i in public_money_dict:
+    public_money_deviation[i]-=contribution
+
+
+
+
