4ac99f74422b1b3f0094f32540115b5a8e0f1893
tonyspecter <tony.specter.choi@gmail.com>
2021-03-30 23:38:52 +0900

    공금입력후 분담금계산 작업중

--- input.py
+++ input.py
@@ -1,11 +1,19 @@
 tot_head=int(input('몇명인가요?  '))
 
 print('명단을 파악하겠습니다')
-name_list=[]
+p_dic={}
+name_list = []
 for i in range(tot_head):
     name=input('이름: ')
     name_list.append(name)
+public_money_dict=dict.fromkeys(name_list)
+
 
 print('공금지불액을 조사하겠습니다')
-for i in range(tot_head):
+for i in public_money_dict:
+    paid_money=int(input('%s :' % i))
+    public_money_dict[i]=paid_money
 
+from collections import Counterunter
+contribution=Counter(public_money_dict)
+print(contribution)
