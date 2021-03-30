2b9ca88f6b69444605514d587e1ff82bd1b9f4a9
tonyspecter <tony.specter.choi@gmail.com>
2021-03-30 20:14:49 +0900

    Initial commit

---
+++ input.py
@@ -0,0 +1,11 @@
+tot_head=int(input('몇명인가요?  '))
+
+print('명단을 파악하겠습니다')
+name_list=[]
+for i in range(tot_head):
+    name=input('이름: ')
+    name_list.append(name)
+
+print('공금지불액을 조사하겠습니다')
+for i in range(tot_head):
+
