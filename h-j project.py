8ed02ae5a47ca0d989e25ae846d4503da587d4af
tonyspecter <tony.specter.choi@gmail.com>
2021-04-03 17:51:55 +0900

    공제금처리 과정중

--- h-j project.py
+++ h-j project.py
@@ -20,8 +20,20 @@ contribution=int(sum/tot_head)
 public_money_deviation=public_money_dict.copy()
 for i in public_money_dict:
     public_money_deviation[i]-=contribution
-print(public_money_deviation)
-#지원아 안녕
+#
+print('공금 일부를 공제 받아야 하는 인원을 조사하겠습니다')
+de_head=int(input())
+
+if de_head>0 :
+    print('공제금을 조사하겠습니다\n이름 금액 :')
+    de_dict = {}
+    deduction_sum=0
+    for i in range(de_head) :
+        name,deduction=input().split()
+        charge=public_money_deviation.get(name)
+        deduction_sum+=deduction
+        new_charge=charge+deduction
+
 
 
 
