33d702d10a5cd6fc21a74dabde07c36395a13e4f
tonyspecter <tony.specter.choi@gmail.com>
2021-04-07 21:16:12 +0900

    입력완료

--- h-j project.py
+++ h-j project.py
@@ -9,19 +9,19 @@ public_money_dict=dict.fromkeys(name_list)
 
 #공금지불액 조사
 print('공금지불액을 조사하겠습니다')
-sum=0
+public_sum=0
 for i in public_money_dict:
     paid_money=int(input('%s :' % i))
-    sum+=paid_money
+    public_sum+=paid_money
     public_money_dict[i]=paid_money
 print(public_money_dict)
 #공급지불액 편차 계산
-contribution=int(sum/tot_head)
+contribution=int(public_sum / tot_head)
 public_money_deviation=public_money_dict.copy()
 for i in public_money_dict:
     public_money_deviation[i]-=contribution
 #
-print('공금 일부를 공제 받아야 하는 인원을 조사하겠습니다')
+print('공금 일부를 공제 받아야 하는 인원수을 조사하겠습니다')
 de_head=int(input())
 
 if de_head>0 :
@@ -30,9 +30,22 @@ if de_head>0 :
     deduction_sum=0
     for i in range(de_head) :
         name,deduction=input().split()
-        charge=public_money_deviation.get(name)
-        deduction_sum+=deduction
+        deduction=int(deduction)
+        charge=public_money_deviation[name]
         new_charge=charge+deduction
+        public_money_deviation[name]=new_charge
+        for p in range(tot_head) :
+            if p==name_list.index(name) :
+                pass
+            else :
+                public_money_deviation[name_list[p]]-=deduction/(tot_head-1)
+
+        deduction_sum += deduction
+    if deduction_sum>public_sum :
+        error
+    print(public_money_deviation)
+
+
 
 
 
